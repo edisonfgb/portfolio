3e62e230ec36d8fd5d07213e0ee401b78da059f2
Edison <edison.gutierrez01@uptc.edu.co>
2026-08-11 20:52:51 -0500

    build(core): configurar dependencias, zod y setup inicial de la app

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,11 +3,25 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes),
+    provideHttpClient(withFetch()),
+    provideClientHydration(withEventReplay()),
+    provideTranslateService({
+      loader: provideTranslateHttpLoader({
+        prefix: '/i18n/',
+        suffix: '.json'
+      }),
+      fallbackLang: 'es',
+      lang: 'es'
+    })
   ]
 };
+
